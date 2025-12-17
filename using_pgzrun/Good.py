ec2d58d6f9175d6f51539c7e81b942c5d773c460
Jeffy <yvxvan68272659@gmail.com>
2025-12-17 13:16:28 +0800

    Good

--- using_pgzrun/Good.py
+++ using_pgzrun/Good.py
@@ -9,7 +9,10 @@ def init(s:any):
     screen=s
 
 class Good:
-    def draw():
+    def draw(self):
+        pass
+
+    def move(self,x:int,y:int):
         pass
 
 class Circle(Good):
@@ -24,6 +27,10 @@ class Circle(Good):
         if self.filled:screen.draw.filled_circle((self.x,self.y),self.radius,self.color)
         else: screen.draw.circle((self.x,self.y),self.radius,self.color)
 
+    def move(self,x:int,y:int):
+        self.x+=x
+        self.y+=y
+
 class Line(Good):
     def __init__(self,x1:int,y1:int,x2:int,y2:int,c:any,w:int=1):
         self.x1=x1
@@ -35,6 +42,12 @@ class Line(Good):
     
     def draw(self):
         pygame.draw.line(screen.draw._surf,self.color,(self.x1,self.y1),(self.x2,self.y2),self.width)
+
+    def move(self,x:int,y:int):
+        self.x1+=x
+        self.y1+=y
+        self.x2+=x
+        self.y2+=y
 
 def draw_goods(lst:List[Good]):
     for good in lst:
