e3b1d0be2e0f1e81bba3ececfe1cd93294fa19ff
Jeffy <yvxvan68272659@gmail.com>
2026-01-17 07:34:14 +0800

    text

---
+++ old_Good/old_run.ps1
@@ -0,0 +1,3 @@
+param([string]$file_name)
+
+C:\Users\Jeffy\Documents\GitHub\ky_pygame\old_Good\old_python\python.exe C:\Users\Jeffy\Documents\GitHub\ky_pygame\old_Good\$file_name
---
+++ using_pygame/Text.py
@@ -0,0 +1,151 @@
+"""The module is to draw Text with pygame easily. (without pgzero)"""
+# by Jeffy
+
+import pygame
+from typing import Any,Tuple
+
+def init(surface:pygame.surface.Surface)->None:
+    """
+    set default surface
+    
+    :param surface: the default surface while call draw without offer surface
+    :type surface: pygame.surface.Surface
+    """
+    Text.surface=surface
+
+class Text:
+    """
+    The class to draw text easily
+    """
+    surface:pygame.surface.Surface|None=None
+
+    def __init__(self,x:int,y:int,text:str,size:int,font_name:str|None=None,bold:bool=False,italic:bool=False,underline:bool=False,antialias:bool=False,color:Any="black",background_color:Any=None):
+        """
+        create a new Text object
+        
+        :param self: object
+        :param x: center pos_x
+        :type x: int
+        :param y: center pos_y
+        :type y: int
+        :param text: text to show
+        :type text: str
+        :param size: font size
+        :type size: int
+        :param font_name: font file or name
+        :type font_name: str | None
+        :param bold: big text
+        :type bold: bool
+        :param italic: spin text
+        :type italic: bool
+        :param underline: underline
+        :type underline: bool
+        :param antialias: without square border
+        :type antialias: bool
+        :param color: font color
+        :type color: Any
+        :param background_color: background color
+        :type background_color: Any
+        """
+        self.font=pygame.font.Font(font_name,size)
+        self.font.bold=bold
+        self.font.italic=italic
+        self.font.underline=underline
+
+        self.x=x
+        self.y=y
+        self.text=text
+        self.size=size
+        self.font_name=font_name
+        self.antialias=antialias
+        self.color=color
+        self.background_color=None if background_color is None else background_color
+        self.set_text_surface()
+        self.rect=None
+
+    def set_size(self,size:int)->None:
+        self.size=size
+        font=pygame.font.Font(self.font_name,size)
+        font.bold=self.font.bold
+        font.italic=self.font.italic
+        font.underline=self.font.underline
+        self.font=font
+        self.set_text_surface()
+
+    def set_text(self,text:str)->None:
+        self.text=text
+        self.set_text_surface()
+
+    def set_color(self,color:Any)->None:
+        self.color=color
+        self.set_text_surface()
+    
+    def set_antialias(self,antialias:bool)->None:
+        self.antialias=antialias
+        self.set_text_surface()
+
+    def set_background_color(self,background_color:Any)->None:
+        self.background_color=None if background_color is None else background_color
+        self.set_text_surface()
+
+    def set_text_surface(self)->None:
+        """
+        create a surface to genorate a Rect object
+        """
+        self.text_surface=self.font.render(
+            self.text,
+            self.antialias,
+            self.color,
+            self.background_color
+        )
+
+    def get_size(self)->Tuple[int,int]:
+        """
+        get the size of the Rect object
+
+        :return: (width,height)
+        :rtype: Tuple[int, int]
+        """
+        return self.text_surface.get_size()
+    
+    def draw(self,surface:pygame.surface.Surface|None=None)->None:
+        """
+        draw the text
+        
+        :param surface: surface, it will be default surface if not offer
+        :type surface: pygame.surface.Surface | None
+        """
+        width,height=self.get_size()
+        if surface is None and Text.surface is None: raise ValueError("surface is still None, please offer aurface or call Text.init(surface) first")
+        self.rect=(Text.surface if surface is None else surface).blit(
+            source=self.text_surface,
+            dest=(self.x-width/2,self.y-height/2),
+            area=None,
+            special_flags=0
+        )
+
+    def __str__(self):
+        items=self.__dict__.items()
+        cls_str=str(self.__class__).split("'")[1]
+        s=f"{cls_str}(\n"
+        for k,v in items:
+            if k in ["rect","font","text_surface"]: continue
+            if k=="text":
+                s+=f"    text={repr(v)},\n"
+                continue
+            s+=f"    {k}={v},\n"
+        s+=")"
+        return s
+
+    def __repr__(self):
+        items=self.__dict__.items()
+        cls_str=str(self.__class__).split("'")[1]
+        s=f"{cls_str}(\n"
+        for k,v in items:
+            if k in ["rect","font","text_surface"]: continue
+            if k=="text":
+                s+=f"    text={repr(v)},\n"
+                continue
+            s+=f"    {k}={v},\n"
+        s+=")"
+        return s
